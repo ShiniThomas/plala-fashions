401711ed98f21826974b432b89c41d3314653aee
ShiniThomas <shinichirachal@gmail.com>
2022-10-19 20:46:10 +0530

    First commit: Create directory for the website

---
+++ src/components/Category/category-item.component.jsx
@@ -0,0 +1,18 @@
+import "./category-item.scss"
+
+const CategoryItem = (props)=>{
+  const {title,imageUrl} = props.category
+  return(
+      <div className="category-container">
+        <div className="background-image" style={{
+          backgroundImage : `url(${imageUrl})`
+        }}/>
+        <div className="category-body-container" >
+          <h2>{title}</h2>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+  )
+}
+
+export default CategoryItem
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,17 @@
+import CategoryItem from "../Category/category-item.component"
+import "./directory.scss"
+
+
+const Directory = ({categories}) =>{
+  
+  return(
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category}/>
+      )
+    )}
+    </div>
+  )
+}
+
+export default Directory
